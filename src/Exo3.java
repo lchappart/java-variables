08125c22e28fff6ed3ada3bb84d66094342e0c8f
Léo Chappart <lchappart>
2025-02-10 12:22:29 +0100

    Exo3

---
+++ src/Exo3.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Exo3 {
+    public static void main(String[] args) throws Exception {
+        int x = 5;
+        double pi = 3.14;
+        String name = "John";
+        char lettre = 'c';
+        boolean vrai = true;
+        byte centVingtSept = 127;
+        short nombreShort = 32767;
+        long nombreLong = 2147483647L;
+        String message = "Hello, World!";
+        int[]  tableau = {1, 2, 3, 4};
+        ArrayList<String> liste = new ArrayList<String>();
+        Date date = new Date();
+        String vide = null;
+        var type = "var";
+    }
+}
