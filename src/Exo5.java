d8bb808eae46e037ed4e86a96a768d608ad56f8f
Léo Chappart <lchappart>
2025-02-10 14:49:56 +0100

    Exo6

---
+++ src/Exo5.java
@@ -0,0 +1,7 @@
+
+public class Exo5 {
+    public static void main(String[] args) throws Exception {
+        int a;
+        System.out.println(a);
+    }
+}
---
+++ src/Exo6.java
@@ -0,0 +1,6 @@
+
+public class Exo6 {
+    public static void main(String[] args) throws Exception {
+        char[] myArray42 = {'q', 'u', 'a', 'r', 'a', 'n', 't', 'e', '-', 'd', 'e', 'u', 'x'};
+    }
+}
