5942fb309a11bf98f35672298fd03a05ac1eee55
Léo Chappart <lchappart>
2025-02-10 12:25:52 +0100

    Exo4

---
+++ src/Exo4.java
@@ -0,0 +1,6 @@
+public class Exo4 {
+    public static void main(String[] args) throws Exception {
+        String my42count = "quarante-deux";
+        System.out.println(my42count.length());
+    }
+}
